fdb2824c15602f40b063afe10d140e36a1cc3448
Salaia <puma.hope@yandex.ru>
2023-04-12 22:49:01 +0300

    Sprint 11-2 checkstyle

--- src/test/java/ru/yandex/practicum/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/FilmorateApplicationTests.java
@@ -1,5 +1,13 @@
 package ru.yandex.practicum;
 
-class FilmoRateApplicationTests {
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmorateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
